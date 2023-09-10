875ab15bd7f78fc82f3f95de34b8a7caf5834699
Balkrishan Singh <balkrishan@192.168.1.5>
2023-09-10 17:04:39 +0530

    added data insertion functions

--- app.py
+++ app.py
@@ -25,7 +25,21 @@ class Student(db):
     admission_date = db.Column(db.DateTime,nullable = False)
     dropout_date = db.Column(db.DateTime)
     dropout_reason = db.Column(db.String(512))
+
+def add_user(name,phone_no,email,password,role):
+    password_hashed = pbkdf2_sha256.hash(password)
+    user = User(name = name, phone_no = phone_no, email = email, password = password_hashed, role = role)
+    db.session.add(user)
+    db.session.commit()
     
+def add_student(roll_no, name,age,gender,caste,school,pincode,admission_date,dropout_date,dropout_reason):
+    student = Student(roll_no = roll_no, name = name, age = age, gender = gender, caste = caste, school = school, pincode = pincode, admission_date = admission_date, dropout_date = dropout_date, dropout_reason = dropout_reason)
+    db.session.add(student)
+    db.session.commit()
+    
+def verify(email,password):
+    user = User.query.filter_by(email).first()
+    return pbkdf2_sha256.verify(password, user.password)
 @app.route("/")
 def root():
     return render_template("index.html")
